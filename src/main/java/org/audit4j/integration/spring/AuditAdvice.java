42c2234a3c73f967932e331ce55ba9dc2575e18d
Janith Bandara <janith3000@gmail.com>
2014-06-23 10:23:45 +0530

    added changes in to pom

--- src/main/java/org/audit4j/integration/spring/AuditAdvice.java
+++ src/main/java/org/audit4j/integration/spring/AuditAdvice.java
@@ -1,9 +1,26 @@
+/*
+ * Copyright 2014 Janith Bandara, This source is a part of Audit4j - 
+ * An open-source audit platform for Enterprise java platform.
+ * http://mechanizedspace.com/audit4j
+ * http://audit4j.org
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *    http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
 package org.audit4j.integration.spring;
 
 import java.lang.reflect.Method;
 
-import org.apache.commons.logging.Log;
-import org.apache.commons.logging.LogFactory;
 import org.audit4j.core.AuditManager;
 import org.springframework.aop.MethodBeforeAdvice;
 
@@ -15,19 +32,12 @@ import org.springframework.aop.MethodBeforeAdvice;
  */
 public class AuditAdvice implements MethodBeforeAdvice {
 
-    /** The log. */
-    private final Log log = LogFactory.getLog(AuditAdvice.class);
-
     /* (non-Javadoc)
      * @see org.springframework.aop.MethodBeforeAdvice#before(java.lang.reflect.Method, java.lang.Object[], java.lang.Object)
      */
     @Override
     public void before(final Method method, final Object[] arg1, final Object arg2) throws Throwable {
-
-        if (log.isDebugEnabled())
-            log.debug("Calling Audit advice before " + method.getName());
-
         AuditManager manager = AuditManager.getInstance();
-        manager.auditWithAnnotation(method.getClass(), method, arg1);
+        manager.audit(method.getClass(), method, arg1);
     }
 }
--- src/main/java/org/audit4j/integration/spring/AuditAspect.java
+++ src/main/java/org/audit4j/integration/spring/AuditAspect.java
@@ -1,9 +1,26 @@
+/*
+ * Copyright 2014 Janith Bandara, This source is a part of Audit4j - 
+ * An open-source audit platform for Enterprise java platform.
+ * http://mechanizedspace.com/audit4j
+ * http://audit4j.org
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *    http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
 package org.audit4j.integration.spring;
 
 import java.lang.reflect.Method;
 
-import org.apache.commons.logging.Log;
-import org.apache.commons.logging.LogFactory;
 import org.aspectj.lang.ProceedingJoinPoint;
 import org.aspectj.lang.annotation.Around;
 import org.aspectj.lang.annotation.Aspect;
@@ -20,9 +37,6 @@ import org.audit4j.core.AuditManager;
  */
 @Aspect
 public class AuditAspect {
-
-    /** The log. */
-    private final Log log = LogFactory.getLog(AuditAspect.class);
 
     /**
      * Execute public method.
@@ -46,7 +60,7 @@ public class AuditAspect {
     	MethodSignature methodSignature = (MethodSignature) pjp.getSignature();
     	Method method = methodSignature.getMethod();
     	
-    	manager.auditWithAnnotation(clazz, method, pjp.getArgs());
+    	manager.audit(clazz, method, pjp.getArgs());
         return pjp.proceed();
     }
 }
